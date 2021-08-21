273700e6b86a15e4271515cce255e4bfe9670fae
LaDane <us.aw@hotmail.com>
2021-08-21 12:49:23 +0200

    Muzzle flashes

--- Project Folder/Weapons/Projectile/PistolProjectile.gd
+++ Project Folder/Weapons/Projectile/PistolProjectile.gd
@@ -1,25 +1,24 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var direction
 var velocity = Vector2()
-var speed = 500
+var speed = 600
+#var on_ready = false
 
 func start(pos, dir):
 	rotation = dir
 	global_position = pos 
 	velocity = Vector2(speed, 0).rotated(rotation)
 
-#func _ready():
-#	look_at(get_global_mouse_position())
-#	direction = (get_global_mouse_position() - global_position).normalized()
-#	velocity = position.direction_to(get_global_mouse_position()) * 10
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	print($AnimatedSprite.animation)
+	$AnimatedSprite.play("flash")
+	
+	
 	
 func _physics_process(delta):
-#	move_and_collide(direction * speed * delta)
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		print("collision")
