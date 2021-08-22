7a9c5f2a1d7ba5aabb3e8b15f6179661bb4ed06c
LaDane <us.aw@hotmail.com>
2021-08-22 11:45:45 +0200

    Robot

--- Project Folder/Characters/Enemies/EnemySpawner.gd
+++ Project Folder/Characters/Enemies/EnemySpawner.gd
@@ -5,6 +5,7 @@ export var spawn_interval = 5
 export var zombie_spawn = true
 export var hitman_spawn = false
 export var shottyman_spawn = false
+export var robot_spawn = false
 
 onready var Player = get_parent().get_parent().get_node("Player")
 var min_spawn_distance = 600
@@ -12,6 +13,7 @@ var min_spawn_distance = 600
 var zombie_scene = preload("res://Characters/Enemies/Zombie.tscn")
 var hitman_scene = preload("res://Characters/Enemies/Hitman.tscn")
 var shottyman_scene = preload("res://Characters/Enemies/Shottyman.tscn")
+var robot_scene = preload("res://Characters/Enemies/Robot.tscn")
 
 func _ready():
 	$Timer.wait_time = spawn_interval
@@ -42,3 +44,7 @@ func _on_Timer_timeout():
 			var shottyman = shottyman_scene.instance()
 			shottyman.position = position 
 			get_parent().add_child(shottyman)
+		if robot_spawn:
+			var robot = robot_scene.instance()
+			robot.position = position 
+			get_parent().add_child(robot)
